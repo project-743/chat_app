39bfb775e2ae7cbbd0f9de4032b2124c8cedef43
HARSHANAND00 <81462084+HARSHANAND00@users.noreply.github.com>
2023-07-17 20:38:40 +0530

    login ipdate

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,65 +5,32 @@ const Home = () => {
   return (
     <div style={{height: '100vh'}}>
         <div className="first" >
-          <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-            <li class="nav-item" role="presentation">
-              <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
-                  aria-controls="pills-login" aria-selected="true">Login</a>
-            </li>
-            <li class="nav-item" role="presentation">
-              <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
-              aria-controls="pills-register" aria-selected="false">Register</a>
-            </li>
-         </ul>
-        <div class="tab-content">
-        <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-        <form>
-            <div class="form-outline mb-4">
-              <input type="email" id="loginName" class="form-control" />
-              <label class="form-label" for="loginName">Email or username</label>
+              <ul class="nav nav-pills">
+              <li class="nav-item pills_mar">
+                <a class="nav-link btn btn-secondary btn-lg active " aria-current="page" href="#">LOGIN</a>
+              </li>
+              <li class="nav-item pills_mar">
+                <a class="nav-link btn btn-secondary btn-lg active " href="#">REGISTER</a>
+              </li>
+            </ul>
+            <form>
+            <div class="form-group enter">
+              <label for="exampleInputEmail1" >Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
             </div>
-            <div class="form-outline mb-4">
-              <input type="password" id="loginPassword" class="form-control" />
-              <label class="form-label" for="loginPassword">Password</label>
+            <div class="form-group enter">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
             </div>
-            <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
-          </form>
-          </div>
-         <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-            <form>   
-            <div class="form-outline mb-4">
-              <input type="text" id="registerName" class="form-control" />
-              <label class="form-label" for="registerName">Name</label>
+            <div class="form-check enter">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
             </div>
-
+            <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
         
-            <div class="form-outline mb-4">
-              <input type="text" id="registerUsername" class="form-control" />
-              <label class="form-label" for="registerUsername">Username</label>
-            </div>
-
-          
-            <div class="form-outline mb-4">
-              <input type="email" id="registerEmail" class="form-control" />
-              <label class="form-label" for="registerEmail">Email</label>
-            </div>
-
-            
-            <div class="form-outline mb-4">
-              <input type="password" id="registerPassword" class="form-control" />
-              <label class="form-label" for="registerPassword">Password</label>
-            </div>
-
-            <div class="form-outline mb-4">
-              <input type="password" id="registerRepeatPassword" class="form-control" />
-              <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-            </div>
-          <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
-          </form>
-        </div>
-        </div>
-        </div>
-        <div className="second" ></div>
+        {/* <div className="second" ></div> */}
     </div>
   );
 };
